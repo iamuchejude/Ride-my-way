25cdb4ad165da127a444880629c36a81fbc647fa
Uche Jude <iamuchejude@gmail.com>
2018-06-15 20:03:48 +0100

    Fix issue on mobile navigation menu [finishes #158400657]

--- UI/js/scripts.js
+++ UI/js/scripts.js
@@ -2,9 +2,11 @@
     'use strict';
     document.querySelector("#date").innerHTML = new Date().getFullYear(); // Set year in footer copyright text
 
-    let menuIcon = document.querySelector('#burger a');
-    menuIcon.addEventListener('click', e => {
-        e.preventDefault();
-        document.querySelector('#menu').classList.toggle('active');
-    })
+    let menuIcon = document.querySelectorAll('a.trigger-menu');
+    menuIcon.forEach(function(el) {
+        el.addEventListener('click', e => {
+            e.preventDefault();
+            document.querySelector('#menu').classList.toggle('active');
+        })
+    });
 })();

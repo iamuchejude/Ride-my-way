2c7a2db9c2c3738ba995ead41c4067824e1b8f57
Uche Jude <iamuchejude@gmail.com>
2018-06-29 05:59:27 +0100

    Removed unwanted codes

--- src/database/connection.js
+++ src/database/connection.js
@@ -4,19 +4,11 @@ const log = require('fancy-log')
 
 env.config();
 
-// const db = new pg.Pool({
-//     user: process.env.DATABASE_USER,
-//     host: process.env.DATABASE_HOST,
-//     database: process.env.DATABASE_NAME,
-//     password: process.env.DATABASE_PASSWORD,
-//     port: process.env.DATABASE_PORT,
-// });
-
 const db = new pg.Pool({
-    user: 'user',
+    user: process.env.DATABASE_USER,
     host: process.env.DATABASE_HOST,
-    database: 'ride@my.way@db',
-    password: 1234,
+    database: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
 });
 
@@ -24,6 +16,4 @@ db.on('error', (err, client) => {
     log.error('Idle client error', err.message, err.stack)
 });
 
-module.exports.query = (text, callback, values=null) => db.query(text, values, callback);
-module.exports.end = () => db.end();
 module.exports = db;

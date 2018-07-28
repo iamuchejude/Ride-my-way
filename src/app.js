41e3ad2e0ddaba2cb8c5e85178b771817f09584c
Uche Jude <iamuchejude@gmail.com>
2018-07-28 12:35:06 +0100

    Update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
+import cors from 'cors';
 import logger from 'morgan';
 import env from 'dotenv';
 import log from 'fancy-log';
@@ -12,13 +14,11 @@ env.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json({ type: 'application/*+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/api/v1/rides', rideRoutes);
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/users', userRoutes);
 
 app.all('/api/v1', (req, res) => {
   res.status(200).json({
@@ -26,6 +26,15 @@ app.all('/api/v1', (req, res) => {
     message: 'Welcome to Ride My Way API',
   })
 })
+
+app.use('/api/v1/rides', rideRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
+
+// Documentation
+app.get('/api/v1/docs', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'docs/index.html'));
+});
 
 app.all('*', (req, res) => {
   res.status(404).json({
